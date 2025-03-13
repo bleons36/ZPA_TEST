5305b8a16d016fb64359529d26994677fb20d146
Sanguk Lee <sulee@expernet.co.kr>
2025-03-13 14:48:37 +0900

    update

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ provider "zpa" {
   client_secret = "-Sl[H]O;#1yu.Mdy70WKC'=$0<V6?bkV"
   zpa_customer_id  = "144131996430368768"
   zpa_cloud  = "PRODUCTION"
+  vanity_domain = "lab.expernet.co.kr"
   use_legacy_client = false
 
 }
