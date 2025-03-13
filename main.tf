7a07a6bce2f2db33cbe69d9fdeaea076fa7c4f66
Sanguk Lee <sulee@expernet.co.kr>
2025-03-13 14:56:59 +0900

    update

--- main.tf
+++ main.tf
@@ -12,12 +12,13 @@ provider "zpa" {
   zpa_client_id = var.zpa_client_id
   zpa_client_secret = var.zpa_client_secret
   zpa_customer_id   = var.zpa_customer_id
- # zpa_cloud  = "PRODUCTION"
- # vanity_domain = "lab.expernet.co.kr"
- # use_legacy_client = false
 
 }
-
+# ZPA Segment Group 생성 (없으면 생성)
+resource "zpa_segment_group" "default" {
+  name        = "Default Segment Group"
+  description = "Automatically created segment group"
+}
 
 # 새로운 ZPA Application Segments 생성
 resource "zpa_application_segment" "app_segment_1" {
@@ -25,6 +26,7 @@ resource "zpa_application_segment" "app_segment_1" {
   description    = "Application Segment for app1.example.com"
   domain_names   = ["app1.example.com"]
   enabled        = true
+  segment_group_id  = zpa_segment_group.default.id  # 필수 추가
 }
 
 # 변수 설정
